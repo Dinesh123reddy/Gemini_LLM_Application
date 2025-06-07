43be4f5d3f157c83fa35a182d168a4f03874de0b
Dinesh Reddy <93877872+Dinesh123reddy@users.noreply.github.com>
2025-06-07 22:56:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
+!pip install google-generativeai
+!pip install streamlit
+!npm install localtunel
+
 import google.generativeai as genai
 import streamlit as st
+
 api_key = "Gemini_api_key"
 genai.configure(api_key=api_key)
 model=genai.GenerativeModel("gemini-1.5-flash-latest")
+
 chat=model.start_chat(history=[])
 st.set_page_config(page_title="Q/A Demo")
 st.header("Mini ChatBot")
@@ -16,9 +22,14 @@ if submit and input:
   response=chat.send_message(input,stream=True)
   st.session_state['chat_history'].append(("You",input))
   st.subheader("The response is :")
+  
   for chunk in response:
     # st.write(chunk)
     st.session_state['chat_history'].append(("Bot",chunk.text))
+
 st.subheader("Chat History")
+
 for role, text in st.session_state['chat_history']:
   st.write(f"{role} : {text}")
+
+!streamlit run /content/app.py & npx localtunnel --port 8501 & curl ipv4.icanhazip.com
